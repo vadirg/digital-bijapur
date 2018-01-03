338d72da338962c105785b5a724d7518c7ff2962
vadi <vadi@gmail.com>
2018-01-03 22:45:54 +0530

    Initial commit of digitalbijapur project

---
+++ src/main/java/com/digitalbijapur/DigitalBijapurApplication.java
@@ -0,0 +1,12 @@
+package com.digitalbijapur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBijapurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalBijapurApplication.class, args);
+	}
+}
---
+++ src/test/java/com/digitalbijapur/digitalbijapur/DigitalBijapurApplicationTests.java
@@ -0,0 +1,16 @@
+package com.digitalbijapur.digitalbijapur;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DigitalBijapurApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
