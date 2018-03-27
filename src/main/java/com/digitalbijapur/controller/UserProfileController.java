3afc649389fd1da83ababfd14c497a31a13bf085
nxa34645 <nxa34645@NXL95103.wbi.nxp.com>
2018-03-28 04:44:43 +0530

    Removing the not required imports.

--- src/main/java/com/digitalbijapur/controller/UserProfileController.java
+++ src/main/java/com/digitalbijapur/controller/UserProfileController.java
@@ -5,7 +5,6 @@ package com.digitalbijapur.controller;
 
 import java.util.List;
 
-import org.json.JSONException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
